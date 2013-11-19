bf4f36e400ba03e7929a533326d63baaa6c09e74
Anna Karpenko <ikarpenok@gmail.com>
2013-11-19 23:02:16 +0200

    Initial commit for tests.

---
+++ src/test/net/problemteam/qrrecognizer/tests/RecognizerServiceTest.java
@@ -0,0 +1,13 @@
+package net.problemteam.qrrecognizer.tests;
+
+import net.problemteam.qrrecognizer.services.RecognizerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RecognizerServiceTest {
+	@Autowired
+	public RecognizerService recognizerService;
+	
+	public void testRecognizeQrCode_normal() throws Exception {
+	}
+}
